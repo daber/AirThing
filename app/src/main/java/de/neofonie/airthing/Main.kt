b771edecacc5d4f7b0e74b0f11e93b9f1fad81f1
Michal Dabrowski <m.a.dabrowski@gmail.com>
2020-09-17 23:28:56 +0200

    Atm display + screen saving added

--- app/src/main/java/de/neofonie/airthing/Main.kt
+++ app/src/main/java/de/neofonie/airthing/Main.kt
@@ -5,11 +5,13 @@ import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import android.view.Window
 import com.ekn.gruzer.gaugelibrary.ArcGauge
 import com.ekn.gruzer.gaugelibrary.HalfGauge
 import com.ekn.gruzer.gaugelibrary.Range
 import com.google.android.things.pio.PeripheralManager
 import com.google.android.things.pio.UartDevice
+import kotlinx.android.synthetic.main.main.*
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import kotlin.concurrent.thread
@@ -20,11 +22,18 @@ class Main : Activity() {
     private lateinit var gaugePm10: ArcGauge
     private lateinit var gaugePm2_5: ArcGauge
     private lateinit var gaugePm1_0: ArcGauge
+    private lateinit var gaugePm10Atm: ArcGauge
+    private lateinit var gaugePm2_5Atm: ArcGauge
+    private lateinit var gaugePm1_0Atm: ArcGauge
     private lateinit var uartDevice: UartDevice
 
     private val handler = Handler()
+    private val dimHandler = Handler()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        window.attributes.screenBrightness = 0.0f
+        window.attributes = window.attributes
         val pm = PeripheralManager.getInstance()
         val uart = pm.uartDeviceList.first()
         uartDevice = pm.openUartDevice(uart)
@@ -34,6 +43,7 @@ class Main : Activity() {
             setStopBits(1)
             setHardwareFlowControl(UartDevice.HW_FLOW_CONTROL_NONE)
         }
+        dimScreenAfterWhile()
 
         thread(isDaemon = true, start = true) {
             while (true) {
@@ -41,14 +51,33 @@ class Main : Activity() {
             }
         }
         setContentView(R.layout.main)
+        root.setOnClickListener {
+            window.attributes.screenBrightness = 1f
+            window.attributes = window.attributes
+            dimScreenAfterWhile()
+        }
         gaugePm1_0 = setChart(R.id.pm1_0, "PM 1.0")
         gaugePm2_5 = setChart(R.id.pm2_5, "PM 2.5")
         gaugePm10 = setChart(R.id.pm10, "PM 10")
+        gaugePm1_0Atm = setChart(R.id.pm1_0atm, "PM 1.0")
+        gaugePm2_5Atm = setChart(R.id.pm2_5atm, "PM 2.5")
+        gaugePm10Atm = setChart(R.id.pm10atm, "PM 10")
+
+    }
+
+    private fun dimScreenAfterWhile() {
+        dimHandler.removeCallbacksAndMessages(null)
+        dimHandler.postDelayed({
+            window.attributes.screenBrightness = 0f
+            window.attributes = window.attributes
+        }, 10 * 1000)
     }
 
 
     @ExperimentalUnsignedTypes
     private fun updateData(byteArray: ByteArray) {
+        val expectedFrameLength = 28
+        val expectedControlSum = 0x424d
         val buffer = ByteBuffer.wrap(byteArray).order(ByteOrder.BIG_ENDIAN).asShortBuffer()
 
         val controlSum = buffer[0].toUShort().toInt()
@@ -56,10 +85,23 @@ class Main : Activity() {
         val pm1_0 = buffer[2].toUShort().toInt()
         val pm2_5 = buffer[3].toUShort().toInt()
         val pm10 = buffer[4].toUShort().toInt()
-        gaugePm2_5.value = pm2_5.toDouble()
-        gaugePm1_0.value = pm1_0.toDouble()
-        gaugePm10.value = pm10.toDouble()
+        val pm1_0atm = buffer[5].toUShort().toInt()
+        val pm2_5atm = buffer[6].toUShort().toInt()
+        val pm10atm = buffer[7].toUShort().toInt()
+        val checkSum = buffer[15].toUShort()
 
+        val sublist = byteArray.map { it.toUByte() }.subList(0, 30)
+        val sum = sublist.map { it.toUShort() }.reduce { acc, uShort -> (acc + uShort).toUShort() }
+        Log.d("CHECKSUM", "$checkSum == $sum")
+        if (controlSum == expectedControlSum && frameLength == expectedFrameLength && checkSum == sum) {
+            gaugePm2_5.value = pm2_5.toDouble()
+            gaugePm1_0.value = pm1_0.toDouble()
+            gaugePm10.value = pm10.toDouble()
+
+            gaugePm2_5Atm.value = pm2_5atm.toDouble()
+            gaugePm1_0Atm.value = pm1_0atm.toDouble()
+            gaugePm10Atm.value = pm10atm.toDouble()
+        }
     }
 
 
@@ -67,7 +109,7 @@ class Main : Activity() {
         val buffer = ByteArray(32)
         val read = uartDevice.read(buffer, buffer.size)
         if (read == 32) {
-            Log.d("UART", "UART$read =  ${buffer.toList()}")
+            Log.d("UART", "UART$read =  ${buffer.toList().map { it.toUByte() }}")
             handler.post { updateData(buffer) }
         }
     }
@@ -98,6 +140,12 @@ class Main : Activity() {
         halfGauge.maxValue = 150.0
         halfGauge.value = 0.0
         return halfGauge
+    }
+
+    override fun onStop() {
+        super.onStop()
+        handler.removeCallbacksAndMessages(null)
+        dimHandler.removeCallbacksAndMessages(null)
     }
 
 }
