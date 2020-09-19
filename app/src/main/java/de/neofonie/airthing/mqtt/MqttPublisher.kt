033b214b880d7204ab57caeeeb47c1fb0ef65e44
Michal Dabrowski <m.a.dabrowski@gmail.com>
2020-09-19 09:25:24 +0200

    Updated version to avoid mqtt persistance bug

--- app/src/main/java/de/neofonie/airthing/mqtt/MqttPublisher.kt
+++ app/src/main/java/de/neofonie/airthing/mqtt/MqttPublisher.kt
@@ -1,25 +1,42 @@
 package de.neofonie.airthing.mqtt
 
 import android.content.Context
+import android.util.Log
 import de.neofonie.airthing.sensor.PMSensor
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import org.eclipse.paho.android.service.MqttAndroidClient
-import org.eclipse.paho.client.mqttv3.MqttMessage
+import org.eclipse.paho.client.mqttv3.*
+import org.eclipse.paho.client.mqttv3.persist.MqttDefaultFilePersistence
 
-class MqttPublisher(ctx: Context, val uri: String) {
+class MqttPublisher(ctx: Context, uri: String) {
     companion object {
         const val TAG = "MQTT"
     }
 
-    val clientId = "test"
-    val client = MqttAndroidClient(ctx, uri, clientId)
+    val connectOptions = MqttConnectOptions().apply {
+        isCleanSession = true
+    }
+    val client = MqttAndroidClient(ctx, uri, "PM Sensor",null as MqttClientPersistence?)
     val coroutineScope = CoroutineScope(Job() + Dispatchers.IO)
 
     fun publish(aFlow: Flow<Map<String, String>>) {
         coroutineScope.launch {
-            client.co { connect() }
+            var attempts =10
+            while(true){
+                try {
+                    delay(1000)
+                    client.co { connect(connectOptions) }
+                    break
+                }catch (e: MqttException){
+                    Log.e(TAG, "Exception while attempting to connect", e)
+                    attempts--
+                    if(attempts == 0){
+                        throw e
+                    }
+                }
+            }
             while (isActive) {
                 aFlow.collect {
                     it.forEach { (k, v) ->
